8b51be6440ee528829cba4cd662607f551f23fbd
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-05-15 17:12:39 +0600

    Catches a value error

--- CS50's Introduction to Programming with Python/04. Exceptions/Classwork/number.py
+++ CS50's Introduction to Programming with Python/04. Exceptions/Classwork/number.py
@@ -1,2 +1,8 @@
-x = int(input("What's x? "))
-print(f"x is{x}")
+# Catches a value error
+
+try:
+    x = int(input("What's x? "))
+    print(f"x is{x}")
+
+except:
+    print("x is not an integer")
