2be8706f2a14a85d8dbad2c3c32b2373040003dd
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-05-19 07:09:10 +0600

    Add files via upload

---
+++ CS50's Introduction to Programming with Python/01. Conditionals/Problem Set 1/classwork/compare1.py
@@ -0,0 +1,23 @@
+a = int(input("What's a?  "))
+b = int(input("What's b?  "))
+
+if a > b or a < b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+
+#code could be imporved
+"""
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
+"""
+
+#following code could be written alternatively
+"""
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is not equal to b")
+"""
