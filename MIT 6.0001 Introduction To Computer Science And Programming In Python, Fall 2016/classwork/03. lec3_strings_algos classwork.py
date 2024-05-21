3f0da5ed254e4a9fb3b678a8458792d5432e1b0f
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-05-21 14:58:17 +0600

    rewritten while loop with a for loop

--- MIT 6.0001 Introduction To Computer Science And Programming In Python, Fall 2016/classwork/03. lec3_strings_algos classwork.py
+++ MIT 6.0001 Introduction To Computer Science And Programming In Python, Fall 2016/classwork/03. lec3_strings_algos classwork.py
@@ -45,24 +45,38 @@
 
 ####################
 ## EXAMPLE: while loops and strings
-## CHALLENGE: rewrite while loop with a for loop
 ####################
-#an_letters = "aefhilmnorsxAEFHILMNORSX"
-#word = input("I will cheer for you! Enter a word: ")
-#times = int(input("Enthusiasm level (1-10): "))
-#
-#i = 0
-#while i < len(word):
+# an_letters = "aefhilmnorsxAEFHILMNORSX"
+# word = input("I will cheer for you! Enter a word: ")
+# times = int(input("Enthusiasm level (1-10): "))
+
+# i = 0
+# while i < len(word):
 #    char = word[i]
 #    if char in an_letters:
 #        print("Give me an " + char + "! " + char)
 #    else:
 #        print("Give me a  " + char + "! " + char)
 #    i += 1
-#print("What does that spell?")
-#for i in range(times):
+# print("What does that spell?")
+# for i in range(times):
 #    print(word, "!!!")
 
+## CHALLENGE: rewrite while loop with a for loop
+an_letters = "aefhilmnorsxAEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ")
+times = int(input("Enthusiasm level (1-10): "))
+
+# if in confusion, go to line 31 & 38
+for char in word:
+   if char in an_letters:
+       print("Give me an " + char + "! " + char)
+   else:
+       print("Give me a  " + char + "! " + char)
+
+print("What does that spell?")
+for i in range(times):
+   print(word, "!!!")
 
     
 ####################
