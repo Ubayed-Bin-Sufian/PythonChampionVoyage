da1e24c3468fb782e11c46006622d53fd3fbf7ea
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-05-23 18:04:46 +0600

    Delete CS50's Introduction to Programming with Python/01. Conditionals/Problem Set 1/classwork directory

--- CS50's Introduction to Programming with Python/01. Conditionals/Problem Set 1/classwork/compare.py
+++
@@ -1,9 +0,0 @@
-a = int(input("What's a?  "))
-b = int(input("What's b?  "))
-
-if a > b:
-    print("a is greater than b")
-elif a < b:
-    print("a is less than b")
-else a == b:
-    print("a is equal to b")
