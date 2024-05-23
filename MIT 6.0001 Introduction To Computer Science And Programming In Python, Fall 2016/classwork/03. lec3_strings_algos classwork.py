ec1079e52d1ca5aea1cc9d85e53464524af03cdb
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-05-23 14:36:49 +0600

    In-class question

--- MIT 6.0001 Introduction To Computer Science And Programming In Python, Fall 2016/classwork/03. lec3_strings_algos classwork.py
+++ MIT 6.0001 Introduction To Computer Science And Programming In Python, Fall 2016/classwork/03. lec3_strings_algos classwork.py
@@ -80,7 +80,7 @@
 #    print(word, "!!!")
 
 
-## Classwork exercises
+## # In-class question
 # The following code includes spaces also
 # s1 = "mit u rock"
 # s2 = "i rule mit"
@@ -130,38 +130,38 @@
 ####################
 ## EXAMPLE: approximate cube root 
 ####################
-cube = 27
-cube = 8120601
-cube = 10000
-epsilon = 0.1
-guess = 0.0
-increment = 0.01
-num_guesses = 0
-# look for close enough answer and make sure
-# didn't accidentally skip the close enough bound
-while abs(guess**3 - cube) >= epsilon and guess <= cube:
-   guess += increment
-   num_guesses += 1
-print('num_guesses =', num_guesses)
-if abs(guess**3 - cube) >= epsilon:
-   print('Failed on cube root of', cube, "with these parameters.")
-else:
-   print(guess, 'is close to the cube root of', cube)
+#cube = 27
+# cube = 8120601
+# #cube = 10000
+# epsilon = 0.1
+# guess = 0.0
+# increment = 0.01
+# num_guesses = 0
+# # look for close enough answer and make sure
+# # didn't accidentally skip the close enough bound
+# while abs(guess**3 - cube) >= epsilon and guess <= cube:
+#    guess += increment
+#    num_guesses += 1
+# print('num_guesses =', num_guesses)
+# if abs(guess**3 - cube) >= epsilon:
+#    print('Failed on cube root of', cube, "with these parameters.")
+# else:
+#    print(guess, 'is close to the cube root of', cube)
 
 
 ####################
 ## EXAMPLE: bisection cube root (only positive cubes!)
 ####################
-#cube = 27
-##cube = 8120601
-## won't work with x < 1 because initial upper bound is less than ans
-##cube = 0.25
-#epsilon = 0.01
-#num_guesses = 0
-#low = 0
-#high = cube
-#guess = (high + low)/2.0
-#while abs(guess**3 - cube) >= epsilon:
+# cube = 27
+# # cube = 8120601
+# # won't work with x < 1 because initial upper bound is less than ans
+# #cube = 0.25
+# epsilon = 0.01
+# num_guesses = 0
+# low = 0  # lower boundary 
+# high = cube  # upper boundary 
+# guess = (high + low)/2.0
+# while abs(guess**3 - cube) >= epsilon:
 #    if guess**3 < cube:
 #        # look only in upper half search space
 #        low = guess
@@ -171,6 +171,15 @@ else:
 #    # next guess is halfway in search space
 #    guess = (high + low)/2.0
 #    num_guesses += 1
-#print('num_guesses =', num_guesses)
-#print(guess, 'is close to the cube root of', cube)
-   
+# print('num_guesses =', num_guesses)
+# print(guess, 'is close to the cube root of', cube)
+ 
+
+# In-class question
+s = "6.00 is 6.0001 and 6.0002"
+new_str = ""
+new_str += s[-1]
+new_str += s[0]
+new_str += s[4::30] 
+new_str += s[13:10:-1]
+print(new_str)
