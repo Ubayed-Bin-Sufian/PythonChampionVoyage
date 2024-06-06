4a804e1d5e53a1050888e35e637ec71a66236e98
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-06 07:27:16 +0600

    Specify with datatype

--- Vizuara Summer of Code/Automatic Code Evaluator Project/Python API/Python and Flask - Routing and Variable Rules.py
+++ Vizuara Summer of Code/Automatic Code Evaluator Project/Python API/Python and Flask - Routing and Variable Rules.py
@@ -16,9 +16,14 @@ def about():
 def blog():
     return "This is the blog page"
 
-@app.route("/blog/<blog_id>")
+# @app.route("/blog/<blog_id>")
+# def blogpost(blog_id):
+#     return "This is the blog post number " + str(blog_id)
+
+# Specifying with datatype
+@app.route("/blog/<string:blog_id>")
 def blogpost(blog_id):
-    return "This is the blog post number " + str(blog_id)
+    return "This is the blog post number " + blog_id
 
 if __name__=="__main__":
     app.run(debug=True)
