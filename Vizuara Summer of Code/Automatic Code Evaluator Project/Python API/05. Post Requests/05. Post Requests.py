cecc6b82dc167a93f0bde63d288716915b5f9ab9
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-06 17:33:52 +0600

    Added source code url

--- Vizuara Summer of Code/Automatic Code Evaluator Project/Python API/05. Post Requests/05. Post Requests.py
+++ Vizuara Summer of Code/Automatic Code Evaluator Project/Python API/05. Post Requests/05. Post Requests.py
@@ -1,9 +1,11 @@
+# Practiced from https://pythonbasics.org/flask-http-methods/#:~:text=By%20default%2C%20the%20Flask%20route,form%20data%20to%20the%20URL.
+
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+   return 'Welcome %s' % name
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
